828560c9a08aedc1d2636031b2ad42118054d838
Shreya Majumdar <shreya.jui.majumdar@gmail.com>
2020-04-26 01:53:05 -0400

    Implementation for the Massachusetts county data sourced from NY Times on GitHub.

--- action.js
+++ action.js
@@ -2,14 +2,17 @@ const path = require("path");
 const update = require("./update");
 
 const WORKSPACE = process.env.GITHUB_WORKSPACE;
-const DATA_REPO = "data"; // from main.yml checkout action path
-const MAIN_REPO = "main"; // from main.yml checkout action path
+// const DATA_REPO = "data"; // from main.yml checkout action path
+// const MAIN_REPO = "main"; // from main.yml checkout action path
+const DATA_REPO = "covid-19-data"; // from main.yml checkout action path
+const MAIN_REPO = "covid19-mass-data"; // from main.yml checkout action path
 
+//The new filenames
+const FILENAME_ALL = "massachusetts.csv";
 const dataPath = path.join(
   WORKSPACE,
   DATA_REPO,
-  "csse_covid_19_data",
-  "csse_covid_19_time_series"
+  FILENAME_ALL
 );
 const outputPath = path.join(WORKSPACE, MAIN_REPO, "docs", "timeseries.json");
 
--- update.js
+++ update.js
@@ -1,66 +1,98 @@
+//Setting up the work environment
 const fs = require("fs");
 const path = require("path");
 const parse = require("csv-parse/lib/sync");
 
-const FILENAME_CONFIRMED = "time_series_covid19_confirmed_global.csv";
-const FILENAME_DEATHS = "time_series_covid19_deaths_global.csv";
-const FILENAME_RECOVERED = "time_series_covid19_recovered_global.csv";
 
-function extract(filepath) {
+// Extract the list of all possible dates and all possible counties from the file
+function extractDatesCounties(filepath) {
   const csv = fs.readFileSync(filepath);
   const [headers, ...rows] = parse(csv);
-  const [province, country, lat, long, ...dates] = headers;
-  const countList = {};
-
-  // HACK: CSVs have different date formats
-  const normalDates = dates.map(date => {
-    const [month, day] = date.split("/");
-    return `2020-${month}-${day}`;
+  const [date, county, state, fips, cases, deaths] = headers;
+  const dateList = [];
+  const countyList = [];
+  
+  rows.forEach(([date, county, state, fips, cases, deaths], idx) => {      //The arrow says for every thing, do that action
+    dateList[date] = date;
+    countyList[county] = county;
   });
 
-  rows.forEach(([province, country, lat, long, ...counts]) => {
-    countList[country] = countList[country] || {};
-    normalDates.forEach((date, i) => {
-      countList[country][date] = countList[country][date] || 0;
-      countList[country][date] += +counts[i];
-    });
-  });
-  return [countList, normalDates];
+  return [Object.keys(countyList), Object.keys(dateList)];
 }
 
-// HACK: Now all the names are the same, but leaving this just in case
-const patchCountryNames = {};
 
+// Initialize a 2-dimensional array so that all counts are initially 0
+function initializeCountList(counties, dates) {
+  const confirmedList = [];
+  const deathsList = [];
+  var count = 0;
+
+  // console.log("initializeCountList:", "All counties", counties, counties.length);
+  // console.log("initializeCountList:", "All Dates", dates, dates.length);
+
+  counties.forEach((county) => {
+    count++;
+    confirmedList[county] = {};
+    deathsList[county] = {};
+
+    dates.forEach((date) => {
+      confirmedList[county][date] = '0';
+      deathsList[county][date] = '0';
+    });
+  });
+
+  // console.log("initializeCountList:", "Count", count);
+  // console.log("initializeCountList:", "Confirmed List", confirmedList);
+  // console.log("initializeCountList:", "Deaths List", deathsList);
+
+  return [confirmedList, deathsList]; 
+}
+
+
+//Reading from the country data method
+function extract(filepath, confirmedList, deathsList) {
+  const csv = fs.readFileSync(filepath);
+  const [headers, ...rows] = parse(csv);
+  const [date, county, state, fips, cases, deaths] = headers;
+
+  rows.forEach(([date, county, state, fips, cases, deaths]) => {      //The arrow says for every thing, do that action
+    confirmedList[county][date] = cases;
+    deathsList[county][date] = deaths;
+  });
+
+  console.log("extract:", "Confirmed List", confirmedList);
+  console.log("extract:", "Deaths List", deathsList);
+
+  return [confirmedList, deathsList];
+}
+
+// Main entry point of this module
 function update(dataPath, outputPath) {
-  const [confirmed, dates] = extract(
-    path.resolve(dataPath, FILENAME_CONFIRMED)
-  );
-  const [deaths] = extract(path.resolve(dataPath, FILENAME_DEATHS));
-  const [recovered] = extract(path.resolve(dataPath, FILENAME_RECOVERED));
-  const countries = Object.keys(confirmed);
+  // Because the data is not present for every county for every date, it can lead to a sparse matrix
+  // We want to find all dates and counties and then initialize a 2-dimentional array
+  const [counties, dates] = extractDatesCounties(dataPath);
+  var [confirmed, deaths] = initializeCountList(counties, dates);
+
+  // This extracts the count from the file
+  [confirmed, deaths] = extract(dataPath, confirmed, deaths);  
+
   const results = {};
 
-  countries.forEach(country => {
-    // Some country names are different in the recovered dataset
-    const recoverdCountry = patchCountryNames[country] || country;
-
-    if (!recovered[recoverdCountry]) {
-      console.warn(`${recoverdCountry} is missing from the recovered dataset`);
-    }
-
-    results[country] = dates.map(date => {
+  //Fixing up the name if there are multiple possible names for an accurate count. ex: US vs USA
+  counties.forEach((county, idx) => {
+    console.log("update:", "Processing", county);
+    //Returning the results in the right format
+    results[county] = dates.map(date => {
       return {
         date,
-        confirmed: confirmed[country][date],
-        deaths: deaths[country][date],
-        recovered:
-          recovered[recoverdCountry] && recovered[recoverdCountry][date] != null
-            ? recovered[recoverdCountry][date]
-            : null
+        confirmed: confirmed[county][date],
+        deaths: deaths[county][date],
+        recovered: null
       };
     });
   });
 
+  console.log("update:", "Geneating output JSON file...");
   fs.writeFileSync(outputPath, JSON.stringify(results, null, 2));
 }
 
